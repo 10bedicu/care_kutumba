de552efa2a6dd343a2f600e183e6d10618988e27
yash-learner <yash.topaz@gmail.com>
2026-04-22 16:05:28 +0530

    Remove patient link related code

--- src/care_kutumba/urls.py
+++ src/care_kutumba/urls.py
@@ -1,10 +1,9 @@
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
-from care_kutumba.api.viewsets import BeneficiaryViewSet, PatientLinkViewSet
+from care_kutumba.api.viewsets import BeneficiaryViewSet
 
 router = DefaultRouter() if settings.DEBUG else SimpleRouter()
 router.register(r"beneficiary", BeneficiaryViewSet, basename="kutumba-beneficiary")
-router.register(r"patient_link", PatientLinkViewSet, basename="kutumba-patient-link")
 
 urlpatterns = router.urls
